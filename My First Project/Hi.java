fb446fa61c4897fade4b956438f22bcd583fef96
Michael-Mihailov <michaelmihailov@hotmail.com>
2022-08-23 15:08:05 -0500

    Learned Blue J and took notes
    hello there

---
+++ My First Project/Hi.java
@@ -0,0 +1,37 @@
+
+/**
+ * Michael Mihailov
+ * 8/23/2022
+ * Hello world program
+ * prints hello world
+ * Who helped me: Elmo
+ */
+public class Hi
+{
+    /**
+     * this is our code notes
+     */
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+        
+        double num = 8.9;
+        int x = 9;
+        boolean choice = false;
+        char letter = 'b'; 
+        float num2 = 3.2f;
+        long num3 = 9000;
+        short num4 = 7;
+        
+        // these are classes and objects
+        // Class  ID      memory  Constructor
+        // Name   
+        Integer   n1 =    new     Integer(47);
+        String    word =  new     String("Michael was here");
+        Double    n2 =    new     Double(6.7);
+        
+        System.out.print(word.length());
+        
+    }
+}
